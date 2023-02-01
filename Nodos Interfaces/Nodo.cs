7e2c4b49694bc6c9638380c396776d64b8e6a769
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-01-31 22:02:44 -0600

    Agregar archivos de proyecto.

---
+++ Nodos Interfaces/Nodo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodos_Interfaces
+{
+    public interface Nodo
+    {
+        string getFuc();
+        float eval(float x);
+    }
+    class NodoHoja : Nodo
+    {
+        public float eval(float x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getFuc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class NodoVar : Nodo
+    {
+        public float eval(float x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getFuc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class NodoInterno : Nodo
+    {
+        string func;
+        int no_hijos;
+        Nodo der, izq;
+
+        public NodoInterno()
+        {
+
+        }
+        public float eval(float x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getFuc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
